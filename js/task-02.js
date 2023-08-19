8a9e180d93a171babafe7aaf98b7ff9d5585fe7e
roLLlyTHuK <ircvipua@gmail.com>
2023-08-19 12:28:06 +0300

    +hw2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngrediens = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.textContent = ingredient;
+  liItem.classList.add('item');
+  listIngrediens.appendChild(liItem);
+});
