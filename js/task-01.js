01a20c5745c09da977bbaf49d519576a43bfc4fd
roLLlyTHuK <ircvipua@gmail.com>
2023-08-22 09:19:39 +0300

    up2

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,21 @@ const categoriesList = document.querySelector('#categories');
 const categoriesItems = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories:`, Number(categoriesItems.length));
 
+//!TEG variations
+
+// categoriesItems.forEach(categoryItem => {
+//   const categoryName = categoryItem.querySelector('h2').textContent;
+//   const categoryElements = categoryItem.querySelectorAll('ul li').length;
+
+//   console.log(`Category:`, categoryName);
+//   console.log(`Elements:`, Number(categoryElements));
+// });
+
+//!DOM variation
 
 categoriesItems.forEach(categoryItem => {
-  const categoryName = categoryItem.querySelector('h2').textContent;
-  const categoryElements = categoryItem.querySelectorAll('ul li').length;
+  const categoryName = categoryItem.firstElementChild.textContent;
+  const categoryElements = categoryItem.children.length;
 
   console.log(`Category:`, categoryName);
   console.log(`Elements:`, Number(categoryElements));
